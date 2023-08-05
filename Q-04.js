37462785e79a17051c1b8ca4647ea835b6ea1026
Rehan Ahmad <rehanathome78@gmail.com>
2023-08-06 04:17:39 +0500

    Escape sequences

---
+++ Q-04.js
@@ -0,0 +1,6 @@
+"use strict";
+// let prsnName: string = " Rehan ";
+// Escape sequences
+// let prsnName: string = "Rehan \nAhmad";
+let prsnName = "Rehan \tAhmad";
+console.log(prsnName);
---
+++ Q-04.ts
@@ -0,0 +1,8 @@
+// let prsnName: string = " Rehan ";
+
+// Escape sequences
+// let prsnName: string = "Rehan \nAhmad";
+
+let prsnName: string = "Rehan \tAhmad";
+
+console.log(prsnName);
