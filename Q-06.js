1e6a2c8772765849e7e001b6a0e1805008b88c6d
Rehan Ahmad <rehanathome78@gmail.com>
2023-08-06 05:33:06 +0500

    PIAIC assignment's

---
+++ Q-06.js
@@ -0,0 +1,4 @@
+"use strict";
+let favNumber = 16;
+let message1 = `My favorite number in all numbers is ${favNumber}.`;
+console.log(message1);
---
+++ Q-06.ts
@@ -0,0 +1,5 @@
+let favNumber: number = 16;
+
+let message1: string = `My favorite number in all numbers is ${favNumber}.`;
+
+console.log(message1);
