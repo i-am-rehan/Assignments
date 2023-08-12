136229500dabb159b30611c9ac4f261d023c7b10
Rehan Ahmad <rehanathome78@gmail.com>
2023-08-12 23:43:20 +0500

    PIAIC assignment's

---
+++ Q-12.js
@@ -0,0 +1,6 @@
+"use strict";
+let guest1 = ["Omer", "Abubakar", "Usman", "Ali"];
+console.log(guest1.pop());
+for (let name of guest1) {
+    console.log(`Aoa ${name}! You are pleased to invite.`);
+}
---
+++ Q-12.ts
@@ -0,0 +1,6 @@
+let guest1:string[] = ["Omer", "Abubakar", "Usman", "Ali"];
+console.log(guest1.pop());
+
+for(let name of guest1){
+    console.log(`Aoa ${name}! You are pleased to invite.`);
+}
