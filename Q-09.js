c40d85fb7aaa8b872f0b064780c5136b628297c4
Rehan Ahmad <rehanathome78@gmail.com>
2023-08-12 18:29:35 +0500

    PIAIC assignment's

---
+++ Q-09.js
@@ -0,0 +1,5 @@
+"use strict";
+let prsnNames = ["Abubakar", "Omer", "Usman"];
+for (let name of prsnNames) {
+    console.log(`Hello, ${name}! Have a great day.`);
+}
---
+++ Q-09.ts
@@ -0,0 +1,5 @@
+let prsnNames:string[] = ["Abubakar", "Omer", "Usman"];
+
+for(let name of prsnNames){
+    console.log(`Hello, ${name}! Have a great day.`);
+}
