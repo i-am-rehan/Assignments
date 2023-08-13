3d8ad8dd8f9df32dd05079cfa5d266fef519e362
Rehan Ahmad <93699662+i-am-rehan@users.noreply.github.com>
2023-08-13 09:28:45 +0500

    Delete Q-12.js

--- Q-12.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let guest1 = ["Omer", "Abubakar", "Usman", "Ali"];
-console.log(guest1.pop());
-for (let name of guest1) {
-    console.log(`Aoa ${name}! You are pleased to invite.`);
-}
