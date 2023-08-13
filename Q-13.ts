f294803145b020aec6f71803d457e667b699fd49
Rehan Ahmad <rehanathome78@gmail.com>
2023-08-13 10:25:31 +0500

    PIAIC assignment's

---
+++ Q-13.ts
@@ -0,0 +1,7 @@
+let guest2:string[] = ["Omer", "Abubakar", "Usman", "Ali"];
+
+for(let name of guest2){
+    console.log(`Aoa ${name}! You are pleased to invite.`);
+
+}
+console.log(`Unfortunately! ${guest2[3]} can't make it to the event.`);
