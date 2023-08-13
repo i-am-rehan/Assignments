d7c6be79f926025438b04715c0543d3a68f29ebe
Rehan Ahmad <93699662+i-am-rehan@users.noreply.github.com>
2023-08-13 09:21:53 +0500

    Delete Q-03.js

--- Q-03.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Famous Quote
-const quote = 'Your time is limited, don\'t waste it living someone else\'s life.';
-// Author of the Quote
-const author = 'Steve Jobs';
-// Printing the Quote and Author
-console.log(`${author} once said, "${quote}"`);
