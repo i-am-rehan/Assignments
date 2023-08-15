512a89ef83837d34341c299796c26906f19796d4
Rehan Ahmad <rehanathome78@gmail.com>
2023-08-15 06:55:01 +0500

    Delete Q-13.js

--- Q-13.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let guest2 = ["Omer", "Abubakar", "Usman", "Ali"];
-for (let name of guest2) {
-    console.log(`Aoa ${name}! You are pleased to invite.`);
-}
-console.log(`Unfortunately! ${guest2[3]} can't make it to the event.`);
